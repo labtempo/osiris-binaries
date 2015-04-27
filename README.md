7dc6ae425c54f04d5fdacc5b41f942b629da30e3
Felipe Santos <live.proto@hotmail.com>
2015-04-27 16:21:46 -0300

    20140427

---
+++ README.md
@@ -0,0 +1,14 @@
+Binários
+======
+
+## Changelog
+
+### 20150427
+
+- blending funcionando no virtualsensornet síncrona e assincronamente
+- adição de exemplos dos módulos de função para o blending
+- adição de um grupo de exemplos de requisições para o virtualsensornet
+- mudanças na api
+- remoção de fila temporária no omcp quando requisitando pelo cliente 
+
+
---
+++ X-Examples/ServerTest/src/main/java/br/uff/labtempo/servertest/VirtualSensorController.java
@@ -0,0 +1,67 @@
+/* 
+ * Copyright 2015 Felipe Santos <fralph at ic.uff.br>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package br.uff.labtempo.servertest;
+
+import br.uff.labtempo.omcp.common.Request;
+import static br.uff.labtempo.omcp.common.RequestMethod.*;
+import br.uff.labtempo.omcp.common.Response;
+import br.uff.labtempo.omcp.common.exceptions.BadRequestException;
+import br.uff.labtempo.omcp.common.exceptions.InternalServerErrorException;
+import br.uff.labtempo.omcp.common.exceptions.MethodNotAllowedException;
+import br.uff.labtempo.omcp.common.exceptions.NotFoundException;
+import br.uff.labtempo.omcp.common.exceptions.NotImplementedException;
+import br.uff.labtempo.omcp.common.utils.ResponseBuilder;
+import br.uff.labtempo.osiris.omcp.Controller;
+
+/**
+ *
+ * @author Felipe Santos <fralph at ic.uff.br>
+ */
+public class VirtualSensorController extends Controller {
+
+    public Response process(Request request) throws MethodNotAllowedException, NotFoundException, InternalServerErrorException, NotImplementedException, BadRequestException {
+        switch (request.getMethod()) {
+            case GET:
+                if (match(request.getResource(), "/sensor/")) {
+                    return get(request);
+                }else if (match(request.getResource(), "/sensor/:id")) {
+                    return getItem(request);
+                }
+                break;
+            case POST:
+                break;
+            case PUT:
+                break;
+            case DELETE:
+                break;
+            case NOTIFY:
+                break;
+        }
+        return goToNext(request);
+
+    }
+
+    private Response get(Request request) {
+        Response response = new ResponseBuilder().ok("corpo todos os sensores").build();
+        return response;
+    }
+
+    private Response getItem(Request request) {
+        Response response = new ResponseBuilder().ok("corpo do sensor 5").build();
+        return response;
+    }
+
+}
