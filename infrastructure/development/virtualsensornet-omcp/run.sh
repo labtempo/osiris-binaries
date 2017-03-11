04ab8542ce0728c7902a107546c032744c3d449b
andre <andreghigo@id.uff.br>
2017-03-11 15:13:28 -0300

    Script de infra de dev parametrizado

--- infrastructure/development/virtualsensornet-omcp/run.sh
+++ infrastructure/development/virtualsensornet-omcp/run.sh
@@ -2,14 +2,14 @@
 VERSION=1.6.0
 
 echo 'postgres.server.db=OsirisVSN' > config.properties
-echo 'postgres.server.ip='$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' db_postgres) >> config.properties
+echo 'postgres.server.ip='$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' osiris-postgresql_dev) >> config.properties
 echo 'postgres.server.port=5432' >> config.properties
 echo 'postgres.user.name=osiris' >> config.properties
 echo 'postgres.user.pass=osiris' >> config.properties
-echo 'rabbitmq.server.ip='$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' rabbitmq) >> config.properties
+echo 'rabbitmq.server.ip='$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' osiris-rabbitmq_dev) >> config.properties
 echo 'rabbitmq.user.name=guest' >> config.properties
 echo 'rabbitmq.user.pass=guest' >> config.properties
 
 cp ../../../virtualsensornet/${VERSION}/VirtualSensorNet-${VERSION}.jar VirtualSensorNet.jar
-docker build -t virtualsensornet-omcp .
-docker run --name virtualsensornet -d -p 8091:8091 virtualsensornet-omcp
+docker build -t osiris-virtualsensornet-omcp_dev .
+docker run --name osiris-virtualsensornet-omcp_dev -d -p 8091:8091 osiris-virtualsensornet-omcp_dev
