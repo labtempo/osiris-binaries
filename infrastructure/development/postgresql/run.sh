edc121bb069c6fd4ea1fdb5dcd0f069099345bf4
andre <andreghigo@id.uff.br>
2017-03-11 19:58:15 -0300

    Ajusta run.sh

--- infrastructure/development/postgresql/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t osiris-postgresql_dev .
-docker run --name osiris-postgresql_dev -d -p 5432:5432 osiris-postgresql_dev
--- infrastructure/development/rabbitmq/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build -t osiris-rabbitmq_dev - < Dockerfile
-docker run --name osiris-rabbitmq_dev -d -it -h localhost -p 5672:5672 -p 15672:15672 -v /opt/rabbitmq:/var/lib/rabbitmq myrabbitmq
