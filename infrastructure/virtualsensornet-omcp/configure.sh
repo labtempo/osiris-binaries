355c2d70a352930dc1d7be3eadc544e09355c090
Andre Ghigo <andre.ghigo@b2wdigital.com>
2017-04-03 18:53:43 -0300

    Adjusting omcp script

--- infrastructure/virtualsensornet-omcp/configure.sh
+++ infrastructure/virtualsensornet-omcp/configure.sh
@@ -12,23 +12,7 @@ VIRTUALSENSORNET_DBNAME=${6:-virtualsensornet}
 POSTGRESQL_USERNAME=${7:-postgres}
 POSTGRESQL_PASSWORD=${8:-postgres}
 
-# export PGPASSWORD=${POSTGRESQL_PASSWORD}
-# psql -h "${POSTGRESQL_IP}" -p 5432 -U "${POSTGRESQL_USERNAME}" -d "${SENSORNET_DBNAME}" --command='\q' > /dev/null
-# if [ $? -ne 0 ]
-# then
-#   echo "ERROR: The PostgreSQL container could not be found with detected IP Address [${POSTGRESQL_IP}] and default port [5342]."
-#   echo "Please provide the PostgreSQL IP Address on Network..."
-#   echo
-#   read POSTGRESQL_IP
-#   psql -h "${POSTGRESQL_IP}" -p 5432 -U "${POSTGRESQL_USERNAME}" -d "${SENSORNET_DBNAME}" --command='\q' > /dev/null
-#   if [ $? -ne 0 ]
-#   then
-#     echo "ERROR: Could not connect to PostgreSQL with provided IP Address [${POSTGRESQL_IP}]. Aborted."
-#     exit 1
-#   fi
-#   echo "INFO: PostgreSQL container found. OK."
-# fi
-# unset PGPASSWORD
+
 
 RABBITMQ_CONTAINER_NAME=${9:-osiris-rabbitmq}
 RABBITMQ_IP=${10:-$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' ${RABBITMQ_CONTAINER_NAME})}
@@ -39,21 +23,7 @@ RABBITMQ_USERNAME=${11:-guest}
 RABBITMQ_PASSWORD=${12:-guest}
 OSIRIS_VIRTUALSENSORNET_JAR_FILE_NAME=VirtualSensorNet-${OSIRIS_VIRTUALSENSORNET_VERSION}.jar
 
-# wget -qO- http://${RABBITMQ_IP}:${RABBITMQ_ADMIN_PORT}/ > /dev/null
-# if [ $? -ne 0 ]
-# then
-#   echo "ERROR: The RabbitMQ container could not be found with detected IP Address [${RABBITMQ_IP}] and default port [${RABBITMQ_PORT}]. Aborted."
-#   echo "Please provide the RabbitMQ IP Address on Network..."
-#   echo
-#   read RABBITMQ_IP
-#   wget -qO- http://${RABBITMQ_IP}:${RABBITMQ_ADMIN_PORT}/ > /dev/null
-#   if [ $? -eq 0 ]
-#   then
-#     echo "ERROR: Could not connect to RabbitMQ with providaded IP Address [${RABBITMQ_IP}]. Aborted."
-#     exit 1
-#   fi
-#   echo "INFO: RabbitMQ container found. OK."
-# fi
+
 
 #Generate the virtualsensornet configuration file
 rm config.properties
